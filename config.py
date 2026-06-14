a6141988f4c7a6eb01438bbcbbf9b31abb987257
kygrott-exe <kygrott@gmail.com>
2026-06-14 23:44:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
 
 class Config:
-    BOT_TOKEN: str  = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN_HERE")
-    API_ID:    int  = int(os.getenv("API_ID", "0"))
-    API_HASH:  str  = os.getenv("API_HASH", "YOUR_API_HASH_HERE")
+     BOT_TOKEN: str  = os.getenv("BOT_TOKEN", "8261194661:AAFdto4jcpBgMh4YmG5zQfuWCK9DTW1WReI")
+    API_ID:    int  = int(os.getenv("API_ID", "29687194"))
+    API_HASH:  str  = os.getenv("API_HASH", "fb286056a72033e9870cacb170b31fcd")
     ADMIN_IDS: list = [
-        int(x) for x in os.getenv("ADMIN_IDS", "0").split(",") if x.strip().isdigit()
+        int(x) for x in os.getenv("ADMIN_IDS", "1899208318").split(",") if x.strip().isdigit()
     ]
     DB_PATH:      str = os.getenv("DB_PATH", "broadcaster.db")
     SESSIONS_DIR: str = os.getenv("SESSIONS_DIR", "accounts")
